142e4529f6b248d60b3302bc363b909fa6ca7cc3
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2025-05-08 12:15:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,12 +23,11 @@ rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/ddns-go
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/lang/golang
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-git clone -b openwrt-23.05 https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy
