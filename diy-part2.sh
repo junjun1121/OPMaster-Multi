e1e01f68574b6c9e2d6c68855dbc563f5cf71831
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2025-08-01 10:43:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,20 +13,21 @@
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/luci/applications/luci-app-openlist
 rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/alist
+rm -rf feeds/packages/net/openlist
 rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/ddns-go
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/net/geoview
+rm -rf feeds/packages/net/tcping
 rm -rf feeds/packages/lang/golang
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
