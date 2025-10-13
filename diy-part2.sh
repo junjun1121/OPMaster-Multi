46fcf1596bda06cc90528d910bfd089fa709ce52
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2025-10-13 15:33:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,7 +25,7 @@ rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/lang/golang
-sed -i 's/192.168.1.1/172.17.17.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/argon-config
